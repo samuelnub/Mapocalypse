e1368f53beac552d050d49964adfd00aa94a644a
Sam Yap (Vivobk) <notsamnub@gmail.com>
2018-09-26 15:01:49 +0100

    first

---
+++ src/consts.js
@@ -0,0 +1,4 @@
+exports.WEB_FILEPATH = __dirname + "/";
+
+exports.MAIN_MENU_WIDTH = 800;
+exports.MAIN_MENU_HEIGHT = 600;
---
+++ src/main.js
@@ -0,0 +1,29 @@
+const electron = require("electron");
+const consts = require("./consts");
+const app = electron.app;
+
+function createWindow() {
+    let window = new electron.BrowserWindow({
+        width: consts.MAIN_MENU_WIDTH,
+        height: consts.MAIN_MENU_HEIGHT
+    });
+    window.loadFile(consts.WEB_FILEPATH + "gui/main-menu.html");
+    window.webContents.openDevTools();
+    window.on("closed", () => {
+        window = null; 
+    });
+};
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if(window === null) {
+        createWindow();
+    }
+});
